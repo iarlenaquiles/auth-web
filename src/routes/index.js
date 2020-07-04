e80fedb0c8cb1df956f7665318549b8fa32a3d93
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-07-04 11:58:59 -0300

    created change password route

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ import Signup from '../pages/Signup';
 import Dashboard from '../pages/Dashboard';
 import User from '../pages/User';
 import ResetPassword from '../pages/ResetPassword';
+import ChangePassword from '../pages/ChangePassword';
 
 export default function Routes() {
   return (
@@ -14,6 +15,7 @@ export default function Routes() {
       <Route path="/" exact component={Signin} />
       <Route path="/register" component={Signup} />
       <Route path="/reset/password" component={ResetPassword}  />
+      <Route path="/auth/reset_password" component={ChangePassword}  />
       <Route path="/dashboard" component={Dashboard} isPrivate />
       <Route path="/users/create" component={User} isPrivate />
       <Route path="/users/edit/:id" component={User} isPrivate />
