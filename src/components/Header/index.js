6a2cdf4f3bac5d1d08815e4a3c8a644a8d743cfd
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-07-03 22:47:25 -0300

    created header

---
+++ src/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { signOut } from '../../store/modules/auth/actions'
+import * as S from './styles';
+
+export default function Header() {
+  const dispatch = useDispatch();
+
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
+  return (
+    <S.Container>
+      <S.Content>
+        <nav>
+          <Link to="/cliente">Clientes</Link>
+          <Link to="/dashboard">Usuários</Link>
+          <Link to="/estoque">Fichas que comprei</Link>
+        </nav>
+
+        <aside>
+          <div>
+            <button onClick={handleSignOut}>Sair</button>
+          </div>
+        </aside>
+      </S.Content>
+    </S.Container>
+  )
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,91 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  background: #fff;
+  padding: 0 30px;
+`;
+
+export const Content = styled.div`
+  height: 64px;
+  max-width: 900px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  nav {
+    display: flex;
+    align-items: center;
+
+    img {
+      margin-right: 20px;
+      padding-right: 20px;
+      border-right: 1px solid #eee;
+    }
+
+    a {
+      font-weight: bold;
+      color: #000;
+      margin-right: 20px;
+    }
+  }
+  aside {
+    display: flex;
+    align-items: center;
+  }
+
+  div {
+    margin-left: 10px;
+    button {
+      display: flex;
+      flex-direction: row;
+      align-self: flex-end;
+      align-items: center;
+      margin: 5px 0 0;
+      height: 44px;
+      padding: 5px;
+      background: #f94d6a;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#F94D6A')};
+      }
+    }
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  margin-left: 20px;
+  padding-left: 20px;
+  border-left: 1px solid #eee;
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #333;
+    }
+
+    a {
+      display: block;
+      margin-top: 2px;
+      font-size: 12px;
+      color: #999;
+    }
+  }
+
+  img {
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+  }
+`;
