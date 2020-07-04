0e3af5d78292dc21f250c6a37fb741f682b8752e
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-07-04 12:27:25 -0300

    created change password page

---
+++ src/pages/ChangePassword/index.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+import * as qs from 'query-string';
+import { changeRequest } from '../../store/modules/auth/actions';
+
+const schema = Yup.object().shape({
+  newPassword:  Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A Senha é obrigatória'),
+  verifyPassword: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A Senha é obrigatória'),
+});
+
+export default function ChangePassword({ location }) {
+  const dispatch = useDispatch();
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    const parsed = qs.parse(location.search);
+    setToken(parsed.token)
+    console.log(token)
+  }, []);
+
+  function handleSubmit({ newPassword, verifyPassword }) {
+    dispatch(changeRequest(newPassword, verifyPassword, token));
+  }
+
+  return (
+    <>
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="newPassword" type="password" placeholder="Sua nova senha" />
+        <Input name="verifyPassword" type="password" placeholder="Confirmação da sua nova senha" />
+
+        <button type="submit">Trocar senha</button>
+      </Form>
+    </>
+  );
+}
