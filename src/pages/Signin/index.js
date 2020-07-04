4991727f8410b25699f2205ffbb159579f783882
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-07-03 22:44:41 -0300

    created signin page

---
+++ src/pages/Signin/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { signInRequest } from '../../store/modules/auth/actions';
+import { Title } from './styles';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A Senha é obrigatória'),
+});
+
+export default function Signin() {
+  const dispatch = useDispatch();
+
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
+
+  return (
+    <>
+      <Title>Tecla T</Title>
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">{loading ? 'Carregando...' : 'Acessar'}</button>
+        <Link to="/register">Criar conta</Link>
+      </Form>
+    </>
+  );
+}
