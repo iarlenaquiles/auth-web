4d39d19addaecbe22ca8cf147f4af7a0cddb258f
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-07-06 15:47:51 -0300

    created people page

---
+++ src/pages/People/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import * as S from './styles';
+import PeopleList from '../../components/PeopleList';
+
+
+export default function People() {
+
+  return (
+    <S.Container>
+      <PeopleList />
+    </S.Container>
+  );
+}
