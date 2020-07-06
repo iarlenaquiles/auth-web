21629198f7326920ddaac1f42c26f723f313b120
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-07-06 15:51:54 -0300

    created starships page

---
+++ src/pages/Starships/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+import StarshipList from '../../components/StarshipList';
+
+export default function Startships() {
+
+  return (
+    <S.Container>
+      <StarshipList />
+    </S.Container>
+  );
+}
