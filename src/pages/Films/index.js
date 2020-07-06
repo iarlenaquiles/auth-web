ce8d5bb472bc02db754b55587d08c0d74af8ffd9
Iarlen Aquiles <iarlensilva775@gmail.com>
2020-07-06 15:47:34 -0300

    created films page

---
+++ src/pages/Films/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './styles';
+import FilmsList from '../../components/FilmsList';
+
+export default function Films() {
+
+  return (
+    <S.Container>
+      <FilmsList />
+    </S.Container>
+  );
+}
